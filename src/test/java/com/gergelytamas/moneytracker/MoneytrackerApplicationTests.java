49db04910b971ff8cc2236dc1172b5c09e79e5ed
Gergely Kiss <gergelytamas.kiss@gmail.com>
2024-07-29 22:42:41 +0200

    Initial commit

---
+++ src/test/java/com/gergelytamas/moneytracker/MoneytrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gergelytamas.moneytracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneytrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
